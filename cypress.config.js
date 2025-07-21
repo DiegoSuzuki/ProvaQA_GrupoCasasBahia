90e74b109c91d20ca5d7fae3f836cd4705ff5efe
Diego de Oliveira Suzuki <diego.adsi@hotmail.com>
2025-07-21 17:54:32 -0300

    Adicionado config para relatório

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,41 @@
-const { defineConfig } = require('cypress');
+// const { defineConfig } = require('cypress');
+
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // Define baseUrl dinamicamente a partir do env
+//       config.baseUrl = config.env.baseUrl;
+//       return config;
+//     },
+//   },
+// });
+
+
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter',
+    mochaJunitReporterReporterOptions: {
+      mochaFile: 'cypress/reports/junit/results-[hash].xml'
+    },
+    cypressMochawesomeReporterReporterOptions: {
+      charts: true,
+      reportPageTitle: 'Relatório de testes',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false
+    }
+  },
+  chromeWebSecurity: false,
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
-      // Define baseUrl dinamicamente a partir do env
+      require('cypress-mochawesome-reporter/plugin')(on)
       config.baseUrl = config.env.baseUrl;
       return config;
+      // implement node event listeners here
     },
   },
 });
